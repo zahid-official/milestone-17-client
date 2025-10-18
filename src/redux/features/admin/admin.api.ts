410fdeb1e46edefa2340945b43677f9f72b2bf2a
zahid-official <zahid.official8@gmail.com>
2025-10-19 00:19:31 +0600

    Update admin dashboard panel

--- src/redux/features/admin/admin.api.ts
+++ src/redux/features/admin/admin.api.ts
@@ -5,24 +5,6 @@ export const adminApi = baseApi.injectEndpoints({
     /*--------------------------
             Mutations
     --------------------------*/
-    // Approve driver
-    approveDriver: builder.mutation({
-      query: (id) => ({
-        url: `/driver/approve/${id}`,
-        method: "PATCH",
-      }),
-      invalidatesTags: ["DRIVER"],
-    }),
-
-    // Reject driver
-    rejectDriver: builder.mutation({
-      query: (id) => ({
-        url: `/driver/reject/${id}`,
-        method: "PATCH",
-      }),
-      invalidatesTags: ["DRIVER"],
-    }),
-
     // Suspend driver
     suspendDriver: builder.mutation({
       query: (id) => ({
@@ -63,34 +45,6 @@ export const adminApi = baseApi.injectEndpoints({
             Queries
     --------------------------*/
     // Driver applications
-    driverApplications: builder.query({
-      query: ({ page = 1, limit = 10, sort, searchTerm }) => {
-        const status = "PENDING";
-        const sortQuery = sort ? `&sort=${sort}` : "";
-        const searchQuery = searchTerm ? `&searchTerm=${searchTerm}` : "";
-
-        return {
-          url: `/driver?page=${page}&limit=${limit}&applicationStatus=${status}${sortQuery}${searchQuery}`,
-          method: "GET",
-        };
-      },
-      providesTags: ["DRIVER"],
-    }),
-
-    // Driver Management
-    manageDrivers: builder.query({
-      query: ({ page = 1, limit = 10, sort, searchTerm }) => {
-        const sortQuery = sort ? `&sort=${sort}` : "";
-        const searchQuery = searchTerm ? `&searchTerm=${searchTerm}` : "";
-
-        return {
-          url: `/driver?page=${page}&limit=${limit}&applicationStatus=APPROVED${sortQuery}${searchQuery}`,
-          method: "GET",
-        };
-      },
-      providesTags: ["DRIVER"],
-    }),
-
     // User Management
     manageUsers: builder.query({
       query: ({ page = 1, limit = 10, sort, searchTerm }) => {
@@ -103,31 +57,13 @@ export const adminApi = baseApi.injectEndpoints({
       },
       providesTags: ["USER"],
     }),
-
-    // Ride Oversight
-    manageRides: builder.query({
-      query: ({ page = 1, limit = 10, sort, searchTerm }) => {
-        const sortQuery = sort ? `&sort=${sort}` : "";
-        const searchQuery = searchTerm ? `&searchTerm=${searchTerm}` : "";
-        return {
-          url: `/ride?page=${page}&limit=${limit}${sortQuery}${searchQuery}`,
-          method: "GET",
-        };
-      },
-      providesTags: ["RIDE"],
-    }),
   }),
 });
 
 export const {
-  useApproveDriverMutation,
-  useRejectDriverMutation,
   useSuspendDriverMutation,
   useUnsuspendDriverMutation,
   useBlockUserMutation,
   useUnblockUserMutation,
-  useDriverApplicationsQuery,
-  useManageDriversQuery,
   useManageUsersQuery,
-  useManageRidesQuery,
 } = adminApi;
--- src/routes/admin/adminSidebar.ts
+++ src/routes/admin/adminSidebar.ts
@@ -3,50 +3,17 @@ import { lazy } from "react";
 
 // Lazy loading
 const Analytics = lazy(() => import("@/pages/admin/Analytics"));
-const AllDriverApplications = lazy(
-  () => import("@/pages/admin/AllDriverApplications")
-);
-const DriverManagement = lazy(() => import("@/pages/admin/DriverManagement"));
-const UserManagement = lazy(() => import("@/pages/admin/UserManagement"));
-const RideOversight = lazy(() => import("@/pages/admin/RideOversight"));
 
 // adminSidebarItems
 const adminSidebarItems: ISidebarItems[] = [
-  // Dashboard
+  // Admin Services
   {
-    title: "Dashboard",
+    title: "Admin Services",
     items: [
       {
         title: "Analytics",
         url: "/admin/analytics",
         component: Analytics,
-      },
-    ],
-  },
-
-  // Data Management
-  {
-    title: "Data Management",
-    items: [
-      {
-        title: "Driver Applications",
-        url: "/admin/driver-applications",
-        component: AllDriverApplications,
-      },
-      {
-        title: "Driver Management",
-        url: "/admin/driver-management",
-        component: DriverManagement,
-      },
-      {
-        title: "User Management",
-        url: "/admin/user-management",
-        component: UserManagement,
-      },
-      {
-        title: "Ride Oversight",
-        url: "/admin/ride-oversight",
-        component: RideOversight,
       },
     ],
   },
