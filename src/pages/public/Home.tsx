c6fe5a31db5e85e76deaf334e5ecb42707bea5e5
zahid-official <zahid.official8@gmail.com>
2025-10-21 14:19:36 +0600

    Add highlight section in home page

--- src/pages/public/Home.tsx
+++ src/pages/public/Home.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/modules/Home/HeroSection";
 import Overview from "@/components/modules/Home/Overview";
+import Services from "@/components/modules/Home/Services";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
-      <Overview/>
+      <Overview />
+      <Services />
     </>
   );
 };
